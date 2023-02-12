45b263ce2e5967158683bb76d6288e8dbfd7b116
ruben <ruben@rubennmg-asus>
2023-02-12 17:03:17 +0100

    cambios desde rama2

--- src/demo2/Demo.java
+++ src/demo2/Demo.java
@@ -3,5 +3,8 @@ package demo2;
 public class Demo {
 	public static void main (String args[]) {
 		System.out.println("Esta es la rama master");
+		
+		/* Cambios desde rama2 */
+		System.out.println("Esta es la rama2");
 	}
 }
